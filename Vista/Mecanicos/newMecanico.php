b24e907b24d10c1ec87a9af5237896c155bd8414
CarlosGp12 <88398558+CarlosGp12@users.noreply.github.com>
2022-07-20 17:32:21 -0500

    Create newMecanico.php

---
+++ Vista/Mecanicos/newMecanico.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <a class="boton regresar">Regresar</a>
+    <main class="contenedor seccion">
+        <form action="">
+            <fieldset>
+                <legend>Datos Mecanico</legend>
+    
+                <label for="nombre">Nombre:</label>
+                <input type="text" id="nombre" name="nombre" placeholder="Nombre del cliente">
+                <label for="apellido">Apellido:</label>
+                <input type="text" id="apellido" name="apellido" placeholder="Apellido del cliente">
+                <label for="telefono">Telefono:</label>
+                <input type="number" id="telefono" name="telefono" placeholder="EJ: 0923377972">
+            </fieldset>
+        </form>
+        <input type="submit" value="Enviar formulario" class="boton">
+    </main>
+</body>
+</html>

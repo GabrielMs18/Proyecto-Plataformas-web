737cd2440c5940948c223704bad7dd3e2d6d05ee
CarlosGp12 <88398558+CarlosGp12@users.noreply.github.com>
2022-07-23 17:57:40 -0500

    Update and rename update.php to updateMecanico.php

--- Vista/Mecanicos/updateMecanico.php
+++ Vista/Mecanicos/updateMecanico.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -56,7 +57,7 @@
     <main class="contenedor seccion">
         <form id="datos">
             <fieldset>
-                <input type="hidden" name="opcion" value="ingresar" />
+                <input type="text" class="form-control" name="opcion" value="actualizar" hidden />
                 <legend>Datos Mecanico</legend>
                 <label for="codigo">Nombre:</label>
                 <input type="number" id="codigo" name="codigo">

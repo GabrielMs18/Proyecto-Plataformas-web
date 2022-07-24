a5a28101e36c31d5803f822698e932b5877958e8
GabrielMs18 <gabriel-montes32@outlook.es>
2022-07-24 11:52:46 -0500

    base

--- taller_juan.sql
+++ taller_juan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-07-2022 a las 23:46:23
+-- Tiempo de generación: 24-07-2022 a las 18:51:19
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -39,9 +39,10 @@ CREATE TABLE `mecanicos` (
 --
 
 INSERT INTO `mecanicos` (`id`, `nombre`, `apellido`, `telefono`) VALUES
-(2, 'carlo', 'parrales', '0958471442'),
-(4, 'Josué Gabr', 'Soriano', '0959144431'),
-(5, 'Diana', 'Campaña', '0965471825');
+(7, 'pepito', 'tomala', '7894651320'),
+(4, 'Josué', 'montesdeoc', '0927599001'),
+(5, 'Diana', 'Campaña', '0965471825'),
+(8, 'pepito', 'pancracio', '0965841651');
 
 -- --------------------------------------------------------
 
@@ -59,15 +60,17 @@ CREATE TABLE `reparacion` (
   `marca` varchar(10) NOT NULL,
   `modelo` varchar(10) NOT NULL,
   `placa` varchar(10) NOT NULL,
-  `fallas` varchar(50) NOT NULL
+  `fallas` varchar(50) NOT NULL,
+  `repuesto` varchar(50) NOT NULL,
+  `precio` int(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `reparacion`
 --
 
-INSERT INTO `reparacion` (`id`, `nombre`, `cedula`, `telefono`, `mecanico`, `vehiculo`, `marca`, `modelo`, `placa`, `fallas`) VALUES
-(2, 'juan', '0927599001', 969144431, 'juan pueblo', 'carro', 'la calle', 'gucci', 'gye 1234', 'no enciende ');
+INSERT INTO `reparacion` (`id`, `nombre`, `cedula`, `telefono`, `mecanico`, `vehiculo`, `marca`, `modelo`, `placa`, `fallas`, `repuesto`, `precio`) VALUES
+(2, 'juansad', '0927599001', 969144431, 'juan pueblo', 'carro', 'la calle', 'gucci', 'gye 1234', 'ya enciende', 'rueda', 15);
 
 -- --------------------------------------------------------
 
@@ -88,7 +91,9 @@ CREATE TABLE `respuestos` (
 --
 
 INSERT INTO `respuestos` (`id`, `nombre`, `descripcion`, `stock`, `precio`) VALUES
-(8, 'rueda', 'componente importante del', 20, 15);
+(10, 'ventana', 'pa cubrirse', 80, 100),
+(9, 'puerta', 'pa cerrarla', 20, 500),
+(8, 'rueda', 'centro del carro', 20, 15);
 
 --
 -- Índices para tablas volcadas
@@ -120,19 +125,19 @@ ALTER TABLE `respuestos`
 -- AUTO_INCREMENT de la tabla `mecanicos`
 --
 ALTER TABLE `mecanicos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `reparacion`
 --
 ALTER TABLE `reparacion`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `respuestos`
 --
 ALTER TABLE `respuestos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
